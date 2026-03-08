94acfe274e20fb53ba3bfdb213c54338929a766b
leirbaglearsi-netizen <leirbaglearsi@gmail.com>
2026-03-08 14:59:48 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// config.js
+window.ENV = {
+  FIREBASE_API_KEY: "AIzaSyBOnaZ7LJL-5_DeK1NAN38IPEzgNqxRoIA",
+  FIREBASE_AUTH_DOMAIN: "payroll-system-test-138a4.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "payroll-system-test-138a4",
+  FIREBASE_STORAGE_BUCKET: "payroll-system-test-138a4.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "884544639724",
+  FIREBASE_APP_ID: "1:884544639724:web:ef78e804e9b4c4f7754bc9"
+};
